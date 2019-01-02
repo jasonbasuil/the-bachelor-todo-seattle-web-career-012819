8304f16d0728a3ba8fb2a380610f501a4f004d2a
Jason Basuil <basuilj@gmail.com>
2019-01-02 02:29:54 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -28,5 +28,6 @@ end
 
 def get_average_age_for_season(data, season)
   season_data = data.fetch(season)
-  season_data.collect
+  season_data.reduce(0) {|sum, contestant
+   + sum + contestant["age"].to_f
 end
